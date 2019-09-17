8223e2b8c5e3e24f55261bf09b7cfed4afc83463
linguohua <lghchinaidea@gmail.com>
2019-09-17 15:32:13 +0800

    print version

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,26 @@ mod auth;
 mod config;
 mod dns;
 mod requests;
-mod tunnels;
 mod service;
+mod tunnels;
 
 use futures::future::lazy;
 use log::info;
 use service::Service;
+use std::env;
 
 fn main() {
     config::log_init().unwrap();
+    let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    if args.len() > 1 {
+        let s = args.get(1).unwrap();
+        if s == "-v" {
+            println!("0.1.0");
+            std::process::exit(0);
+        }
+    }
+
     info!("try to start lproxy-cv server..");
 
     let l = lazy(|| {
