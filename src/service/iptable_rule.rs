900ee921d0f14a3a7a8c19fc6b8f4a33e17862a4
linguohua <lghchinaidea@gmail.com>
2019-10-03 12:51:30 +0800

    openwrt bug: iptables match socket failed

--- src/service/iptable_rule.rs
+++ src/service/iptable_rule.rs
@@ -5,14 +5,24 @@ pub fn set_iptables_rules() {
     unset_iptables_rules();
     ipset::set_ipset();
 
-    let args = "iptables -t mangle -N LPROXY_TCP;\
-                iptables -t mangle -A LPROXY_TCP -p tcp -j TPROXY --on-port 5000 --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
-                iptables -t mangle -N DIVERT;\
-                iptables -t mangle -A DIVERT -j MARK --set-mark 1;\
-                iptables -t mangle -A DIVERT -j ACCEPT;\
-                iptables -t mangle -I PREROUTING -p tcp -m set --match-set LPROXY dst -j LPROXY_TCP;\
-                iptables -t mangle -I PREROUTING -p tcp -m socket -j DIVERT";
+    let args =
+        "iptables -t mangle -N LPROXY_TCP;\
+            iptables -t mangle -A LPROXY_TCP -p tcp -j TPROXY --on-port 5000 --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
+            iptables -t mangle -I PREROUTING -p tcp -m set --match-set LPROXY dst -j LPROXY_TCP;\
+            iptables -t mangle -I PREROUTING -d 127.0.0.0/24 -j RETURN;\
+            iptables -t mangle -I PREROUTING -d 192.168.0.0/16 -j RETURN;\
+            iptables -t mangle -I PREROUTING -d 10.42.0.0/16 -j RETURN;\
+            iptables -t mangle -I PREROUTING -d 0.0.0.0/8 -j RETURN;\
+            iptables -t mangle -I PREROUTING -d 10.0.0.0/8 -j RETURN;\
+            iptables -t mangle -I PREROUTING -d 172.16.0.0/12 -j RETURN;\
+            iptables -t mangle -I PREROUTING -d 224.0.0.0/4 -j RETURN;\
+            iptables -t mangle -I PREROUTING -d 240.0.0.0/4 -j RETURN;\
+            iptables -t mangle -I PREROUTING -d 169.254.0.0/16 -j RETURN";
 
+    // iptables -t mangle -N DIVERT;\
+    // iptables -t mangle -A DIVERT -j MARK --set-mark 1;\
+    // iptables -t mangle -A DIVERT -j ACCEPT;\
+    // iptables -t mangle -I PREROUTING -p tcp -m socket -j DIVERT
     // enable prerouting for CHAIN DIVERT
     match do_bash_cmd(args) {
         Ok(_) => {}
@@ -21,12 +31,22 @@ pub fn set_iptables_rules() {
 }
 
 pub fn unset_iptables_rules() {
-    let args = "iptables -t mangle -D PREROUTING -p tcp -m socket -j DIVERT;\
-                iptables -t mangle -D PREROUTING -p tcp -m set --match-set LPROXY dst -j LPROXY_TCP;\
-                iptables -t mangle -F DIVERT;\
-                iptables -t mangle -X DIVERT;\
-                iptables -t mangle -F LPROXY_TCP;\
-                iptables -t mangle -X LPROXY_TCP";
+    let args =
+        "iptables -t mangle -D PREROUTING -d 127.0.0.0/24 -j RETURN;\
+         iptables -t mangle -D PREROUTING -d 192.168.0.0/16 -j RETURN;\
+         iptables -t mangle -D PREROUTING -d 10.42.0.0/16 -j RETURN;\
+         iptables -t mangle -D PREROUTING -d 0.0.0.0/8 -j RETURN;\
+         iptables -t mangle -D PREROUTING -d 10.0.0.0/8 -j RETURN;\
+         iptables -t mangle -D PREROUTING -d 172.16.0.0/12 -j RETURN;\
+         iptables -t mangle -D PREROUTING -d 224.0.0.0/4 -j RETURN;\
+         iptables -t mangle -D PREROUTING -d 240.0.0.0/4 -j RETURN;\
+         iptables -t mangle -D PREROUTING -d 169.254.0.0/16 -j RETURN;\
+         iptables -t mangle -D PREROUTING -p tcp -m set --match-set LPROXY dst -j LPROXY_TCP;\
+         iptables -t mangle -F LPROXY_TCP;\
+         iptables -t mangle -X LPROXY_TCP";
+
+    // iptables -t mangle -F DIVERT;\
+    // iptables -t mangle -X DIVERT;\
 
     match do_bash_cmd(args) {
         Ok(_) => {}
