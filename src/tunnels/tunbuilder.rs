e82f76d55309bc70d69e34a9b6fb6bd1b6cb8d28
linguohua <lghchinaidea@gmail.com>
2019-10-09 23:07:24 +0800

    TLS1.3

--- src/tunnels/tunbuilder.rs
+++ src/tunnels/tunbuilder.rs
@@ -1,25 +1,28 @@
-use crate::config::DEFAULT_REQ_QUOTA;
 use super::TunMgr;
 use super::Tunnel;
+use crate::config::DEFAULT_REQ_QUOTA;
+use crate::lws;
 use futures::{Future, Stream};
 use log::{debug, error, info};
+use native_tls::TlsConnector;
 use nix::sys::socket::{shutdown, Shutdown};
 use std::cell::RefCell;
+use std::os::unix::io::AsRawFd;
+use std::os::unix::io::RawFd;
 use std::rc::Rc;
 use tokio;
 use tokio::runtime::current_thread;
 use url;
-use native_tls::TlsConnector;
-use crate::lws;
-use std::os::unix::io::AsRawFd;
-use std::os::unix::io::RawFd;
 
 pub fn connect(tm: &TunMgr, mgr2: Rc<RefCell<TunMgr>>, index: usize) {
     let relay_domain = &tm.relay_domain;
     let relay_port = tm.relay_port;
     let ws_url = &tm.url;
     let tunnel_req_cap = tm.tunnel_req_cap;
-    let ws_url = format!("{}?cap={}&quota={}", ws_url, tunnel_req_cap, DEFAULT_REQ_QUOTA);
+    let ws_url = format!(
+        "{}?cap={}&quota={}",
+        ws_url, tunnel_req_cap, DEFAULT_REQ_QUOTA
+    );
     let url = url::Url::parse(&ws_url).unwrap();
 
     let mgr1 = mgr2.clone();
@@ -64,10 +67,7 @@ pub fn connect(tm: &TunMgr, mgr2: Rc<RefCell<TunMgr>>, index: usize) {
             });
 
             let rx = rx.map_err(|_| {
-                std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    "[tunbuilder] rx-shit",
-                )
+                std::io::Error::new(std::io::ErrorKind::Other, "[tunbuilder] rx-shit")
             });
 
             let send_fut = rx.forward(sink);
@@ -100,9 +100,19 @@ pub fn connect(tm: &TunMgr, mgr2: Rc<RefCell<TunMgr>>, index: usize) {
 }
 
 pub type FrameType = lws::LwsFramed<tokio_tls::TlsStream<tokio_tcp::TcpStream>>;
-pub fn ws_connect_async(relay_domain:&str, relay_port:u16, url2: url::Url) -> impl Future<Item=(FrameType, RawFd), Error=std::io::Error> {
-    let cx = TlsConnector::builder().build().unwrap();
+pub fn ws_connect_async(
+    relay_domain: &str,
+    relay_port: u16,
+    url2: url::Url,
+) -> impl Future<Item = (FrameType, RawFd), Error = std::io::Error> {
+    let mut builder = TlsConnector::builder();
+    let builder = builder.min_protocol_version(Some(native_tls::Protocol::Tlsv13));
+    #[cfg(not(target_arch = "x86_64"))]
+    let builder = builder.select_cipher_suit(Some("TLS_CHACHA20_POLY1305_SHA256".to_string()));
+    let cx = builder.build().unwrap();
+
     let cx = tokio_tls::TlsConnector::from(cx);
+
     let host_str = url2.host_str().unwrap().to_string();
     let path = url2.path().to_string();
     let query = match url2.query() {
@@ -113,29 +123,27 @@ pub fn ws_connect_async(relay_domain:&str, relay_port:u16, url2: url::Url) -> im
     let path = format!("{}{}", path, query);
     info!("ws_connect_async, host:{}, path:{}", host_str, path);
 
-    let fut = tokio_dns::TcpStream::connect((relay_domain, relay_port))
-        .and_then(move |socket| {
-            let rawfd = socket.as_raw_fd();
-            let tls_handshake = cx.connect(&host_str, socket);
-            let fut = tls_handshake
-                .map_err(|tslerr| {
-                    println!("TLS connect error:{}", tslerr);
-                    std::io::Error::from(std::io::ErrorKind::NotConnected)
-                })
-                .and_then(move |socket| {
-                    let handshake =
-                        lws::do_client_hanshake(socket, &host_str, &path);
-                    let handshake = handshake.and_then(move |(lsocket, tail)| {
-                        println!("lws handshake completed");
-                        let framed = lws::LwsFramed::new(lsocket, tail);
+    let fut = tokio_dns::TcpStream::connect((relay_domain, relay_port)).and_then(move |socket| {
+        let rawfd = socket.as_raw_fd();
+        let tls_handshake = cx.connect(&host_str, socket);
+        let fut = tls_handshake
+            .map_err(|tslerr| {
+                println!("TLS connect error:{}", tslerr);
+                std::io::Error::from(std::io::ErrorKind::NotConnected)
+            })
+            .and_then(move |socket| {
+                let handshake = lws::do_client_hanshake(socket, &host_str, &path);
+                let handshake = handshake.and_then(move |(lsocket, tail)| {
+                    println!("lws handshake completed");
+                    let framed = lws::LwsFramed::new(lsocket, tail);
 
-                        Ok((framed, rawfd))
-                    });
-
-                    handshake
+                    Ok((framed, rawfd))
                 });
 
-            fut
-        });
+                handshake
+            });
+
+        fut
+    });
     fut
 }
