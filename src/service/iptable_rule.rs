50c66137644fc862db2cccfb6f569da9191b4e12
linguohua <lghchinaidea@gmail.com>
2020-05-21 22:55:27 +0800

    use format to construct iptables comand line

--- src/service/iptable_rule.rs
+++ src/service/iptable_rule.rs
@@ -1,29 +1,30 @@
 use super::ip_rules::do_bash_cmd;
 // use super::ipset;
+use crate::config::{LOCAL_TPROXY_SERVER_PORT};
 
 pub fn set_iptables_rules() {
     unset_iptables_rules();
 
-    let args =
+    let args = format!(
         "iptables -t mangle -N LPROXY_TCP;\
-            iptables -t mangle -A LPROXY_TCP -p tcp -j TPROXY --on-port 5000 --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
+            iptables -t mangle -A LPROXY_TCP -p tcp -j TPROXY --on-port {ppport} --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
             iptables -t mangle -I PREROUTING -p tcp -m set --match-set LPROXYN dst -j LPROXY_TCP;\
             iptables -t mangle -I PREROUTING -p tcp -m set --match-set LPROXY dst -j LPROXY_TCP;\
-            iptables -t mangle -A LPROXY_TCP -p udp -j TPROXY --on-port 5000 --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
+            iptables -t mangle -A LPROXY_TCP -p udp -j TPROXY --on-port {ppport} --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
             iptables -t mangle -I PREROUTING -p udp -m set --match-set LPROXYN dst -j LPROXY_TCP;\
             iptables -t mangle -I PREROUTING -p udp -m set --match-set LPROXY dst -j LPROXY_TCP;\
             ip6tables -t mangle -N LPROXY_TCP;\
-            ip6tables -t mangle -A LPROXY_TCP -p tcp -j TPROXY --on-port 5000 --on-ip ::1 --tproxy-mark 0x01/0x01;\
+            ip6tables -t mangle -A LPROXY_TCP -p tcp -j TPROXY --on-port {ppport} --on-ip ::1 --tproxy-mark 0x01/0x01;\
             ip6tables -t mangle -I PREROUTING -p tcp -m set --match-set LPROXY6 dst -j LPROXY_TCP;\
-            ip6tables -t mangle -A LPROXY_TCP -p udp -j TPROXY --on-port 5000 --on-ip ::1 --tproxy-mark 0x01/0x01;\
-            ip6tables -t mangle -I PREROUTING -p udp -m set --match-set LPROXY6 dst -j LPROXY_TCP;";
+            ip6tables -t mangle -A LPROXY_TCP -p udp -j TPROXY --on-port {ppport} --on-ip ::1 --tproxy-mark 0x01/0x01;\
+            ip6tables -t mangle -I PREROUTING -p udp -m set --match-set LPROXY6 dst -j LPROXY_TCP;", ppport = LOCAL_TPROXY_SERVER_PORT);
 
     // iptables -t mangle -N DIVERT;\
     // iptables -t mangle -A DIVERT -j MARK --set-mark 1;\
     // iptables -t mangle -A DIVERT -j ACCEPT;\
     // iptables -t mangle -I PREROUTING -p tcp -m socket -j DIVERT
     // enable prerouting for CHAIN DIVERT
-    match do_bash_cmd(args) {
+    match do_bash_cmd(&args) {
         Ok(_) => {}
         Err(_) => {}
     }
@@ -57,7 +58,7 @@ pub fn set_iptables_rules_for_global() {
     unset_iptables_rules_for_global();
 
     // TODO: fix ipv6
-    let args =
+    let args = format!(
         "iptables -t mangle -N LPROXY_TCP;\
             iptables -t mangle -A LPROXY_TCP -d 0.0.0.0/8 -j RETURN;\
             iptables -t mangle -A LPROXY_TCP -d 10.0.0.0/8 -j RETURN;\
@@ -67,17 +68,17 @@ pub fn set_iptables_rules_for_global() {
             iptables -t mangle -A LPROXY_TCP -d 192.168.0.0/16 -j RETURN;\
             iptables -t mangle -A LPROXY_TCP -d 224.0.0.0/4 -j RETURN;\
             iptables -t mangle -A LPROXY_TCP -d 240.0.0.0/4 -j RETURN;\
-            iptables -t mangle -A LPROXY_TCP -p tcp -j TPROXY --on-port 5000 --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
+            iptables -t mangle -A LPROXY_TCP -p tcp -j TPROXY --on-port {ppport} --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
             iptables -t mangle -I PREROUTING -p tcp -j LPROXY_TCP;\
-            iptables -t mangle -A LPROXY_TCP -p udp -j TPROXY --on-port 5000 --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
-            iptables -t mangle -I PREROUTING -p udp -j LPROXY_TCP;";
+            iptables -t mangle -A LPROXY_TCP -p udp -j TPROXY --on-port {ppport} --on-ip 127.0.0.1 --tproxy-mark 0x01/0x01;\
+            iptables -t mangle -I PREROUTING -p udp -j LPROXY_TCP;", ppport = LOCAL_TPROXY_SERVER_PORT);
 
     // iptables -t mangle -N DIVERT;\
     // iptables -t mangle -A DIVERT -j MARK --set-mark 1;\
     // iptables -t mangle -A DIVERT -j ACCEPT;\
     // iptables -t mangle -I PREROUTING -p tcp -m socket -j DIVERT
     // enable prerouting for CHAIN DIVERT
-    match do_bash_cmd(args) {
+    match do_bash_cmd(&args) {
         Ok(_) => {}
         Err(_) => {}
     }
